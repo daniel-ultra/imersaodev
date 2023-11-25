1296745352e8790aa0a9bd7e14713854bd70ce48
Daniel <151851671+daniel-ultra@users.noreply.github.com>
2023-11-25 11:45:16 -0300

    Update conversor.js

--- Aula 01/conversor.js
+++ Aula 01/conversor.js
@@ -1,1 +1,12 @@
+var nomeUsuario = prompt("Olá! Este é o conversor interestelar. Como você se chama?")
 
+var distAnoLuz = parseFloat(prompt(nomeUsuario + ", Digite um valor em anos-luz e o converteremos para metros!"));
+
+var conversaoAnosLuzMetros = 9.461 * 10 ** 15;
+
+var distanciaEmMetros = distAnoLuz * conversaoAnosLuzMetros;
+
+alert(nomeUsuario + ", temos seu resultado! A distância da estrela Sirius para a Terra é de " +
+    distanciaEmMetros +
+    " metros."
+);
