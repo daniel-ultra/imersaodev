0d0cf808c6c26f656469f94283652e20c721b64c
Daniel <151851671+daniel-ultra@users.noreply.github.com>
2023-11-25 16:44:29 -0300

    Update desafio4_mentalista.js
    Incluído um identificador de rodadas não jogadas.

--- Aula 02/desafio4_mentalista.js
+++ Aula 02/desafio4_mentalista.js
@@ -6,11 +6,14 @@ var numeroSecreto = randomRange(1, 1000)
 
 var rodadas = 0
 
+// Utilizando um laço de "while":
 while(numeroSecreto != numeroEscolhido) {
   var numeroEscolhido = prompt("Escolha um número entre 1 e 1000.")
   if(numeroSecreto == numeroEscolhido) {
     alert("Monstro!!!! Você acertou!")
     rodadas = rodadas + 1
+  } else if(isNaN(numeroEscolhido) || numeroEscolhido === null || numeroEscolhido.trim() === '') {
+    alert("Você não escolheu um número! Esta rodada não será contabilizada.")
   } else if(numeroSecreto > numeroEscolhido) {
     alert("Errou! " + numeroEscolhido + " é menor que o número secreto.")
     rodadas = rodadas + 1
