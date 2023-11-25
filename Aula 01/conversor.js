c49f0b9812469e69bd1fd976b8768e3da833e24e
Daniel <151851671+daniel-ultra@users.noreply.github.com>
2023-11-25 16:12:19 -0300

    Update conversor.js
    Comentários inseridos.

--- Aula 01/conversor.js
+++ Aula 01/conversor.js
@@ -2,16 +2,19 @@ var nomeUsuario = prompt(
   "Olá! Este é o conversor interestelar. Como você se chama?"
 );
 
+// Caso o usuário não digite seu nome ou apenas preencha o prompt com espaços vazios:
 while (nomeUsuario === null || nomeUsuario.trim() === "") {
   nomeUsuario = prompt(
     "Parece que você não digitou seu nome. Por favor, digite um nome válido:"
   );
 }
 
+// Função que coloca a primeira letra do nome em maiúscula:
 function majNomeUsuario(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+// Coletando a quantidade a ser convertida e transformando-a em float:
 var distAnoLuz = parseFloat(
   prompt(
     majNomeUsuario(nomeUsuario) +
@@ -19,12 +22,14 @@ var distAnoLuz = parseFloat(
   )
 );
 
+// Verificando se a quantidade introduzida de fato é um número:
 while (isNaN(distAnoLuz)) {
   var distAnoLuz = prompt(
     "Você não inseriu um número válido. Por favor, tente novamente."
   );
 }
 
+// Constante de conversão:
 var conversaoAnosLuzMetros = 9.461 * 10 ** 15;
 
 var distanciaEmMetros = distAnoLuz * conversaoAnosLuzMetros;
