c58522d6fefb2a599399a957596b2c0856b6cbf2
Daniel <151851671+daniel-ultra@users.noreply.github.com>
2023-11-25 13:27:08 -0300

    Update conversor.js

--- Aula 01/conversor.js
+++ Aula 01/conversor.js
@@ -1,12 +1,39 @@
-var nomeUsuario = prompt("Olá! Este é o conversor interestelar. Como você se chama?")
+var nomeUsuario = prompt(
+  "Olá! Este é o conversor interestelar. Como você se chama?"
+);
 
-var distAnoLuz = parseFloat(prompt(nomeUsuario + ", Digite um valor em anos-luz e o converteremos para metros!"));
+while (nomeUsuario === null || nomeUsuario.trim() === "") {
+  nomeUsuario = prompt(
+    "Parece que você não digitou seu nome. Por favor, digite um nome válido:"
+  );
+}
+
+function majNomeUsuario(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+var maiusculaNomeUsuario = majNomeUsuario(nomeUsuario);
+
+var distAnoLuz = parseFloat(
+  prompt(
+    maiusculaNomeUsuario +
+      ", digite um valor em anos-luz e o converteremos para metros!"
+  )
+);
+
+while (isNaN(distAnoLuz) === true) {
+  var distAnoLuz = prompt(
+    "Você não inseriu um número válido. Por favor, tente novamente."
+  );
+}
 
 var conversaoAnosLuzMetros = 9.461 * 10 ** 15;
 
 var distanciaEmMetros = distAnoLuz * conversaoAnosLuzMetros;
 
-alert(nomeUsuario + ", temos seu resultado! A distância da estrela Sirius para a Terra é de " +
+alert(
+  maiusculaNomeUsuario +
+    ", temos seu resultado! A distância da estrela Sirius para a Terra é de " +
     distanciaEmMetros +
     " metros."
 );
