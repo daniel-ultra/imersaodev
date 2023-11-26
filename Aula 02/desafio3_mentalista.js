fa2ffbaf626d6c058570231f8da8eac53f7522de
Daniel <151851671+daniel-ultra@users.noreply.github.com>
2023-11-26 14:59:23 -0300

    Update desafio3_mentalista.js

--- Aula 02/desafio3_mentalista.js
+++ Aula 02/desafio3_mentalista.js
@@ -1,1 +1,49 @@
+// Desafio 3: Limitar o número máximo de tentativas para acertar o número secreto.
 
+function randomRange(myMin, myMax) {
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+};
+
+var numeroSecreto = randomRange(1, 1000)
+
+var rodadas = 0
+
+for(i = 0; numeroSecreto == numeroEscolhido || rodadas == 10; i++) {
+  var numeroEscolhido = prompt("Escolha um número entre 1 e 1000. Você terá no máximo 10 tentaivas para acertar o número!")
+  if(isNaN(numeroEscolhido) || numeroEscolhido === null || numeroEscolhido.trim() === "" || numeroEscolhido <= 0) {
+    alert("Você não escolheu um número! Esta rodada não será contabilizada.");
+  } else if(numeroEscolhido == numeroSecreto) {
+    alert("Monstro!!!! Você acertou!")
+    rodadas ++
+    alert("Você demorou " + rodadas + " rodadas para acertar o número.")
+  } else if(numeroSecreto > numeroEscolhido) {
+    alert("Errou! " + numeroEscolhido + " é menor que o número secreto.")
+    rodadas ++
+  } else if(numeroSecreto < numeroEscolhido) {
+    alert("Errou! " + numeroEscolhido + " é maior que o número secreto.")
+    rodadas ++
+  }
+}
+
+if(numeroSecreto != numeroEscolhido && rodadas == 10) {
+  alert("Você atingiu o número máximo de tentativas.")
+}
+
+// Utilizando um laço de "while":
+while(numeroSecreto != numeroEscolhido) {
+  var numeroEscolhido = prompt("Escolha um número entre 1 e 1000.")
+  if(numeroSecreto == numeroEscolhido) {
+    alert("Monstro!!!! Você acertou!")
+    rodadas = rodadas + 1
+  } else if(isNaN(numeroEscolhido) || numeroEscolhido === null || numeroEscolhido.trim() === '' || numeroEscolhido <= 0) {
+    alert("Você não escolheu um número! Esta rodada não será contabilizada.")
+  } else if(numeroSecreto > numeroEscolhido) {
+    alert("Errou! " + numeroEscolhido + " é menor que o número secreto.")
+    rodadas = rodadas + 1
+  } else if(numeroSecreto < numeroEscolhido) {
+    alert("Errou! " + numeroEscolhido + " é maior que o número secreto.")
+    rodadas = rodadas + 1
+  }
+}
+
+alert("Você demorou " + rodadas + " rodadas para acertar o número.")
