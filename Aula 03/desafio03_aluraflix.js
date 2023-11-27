10b666f021240aeb6ec95a41900e8205ba78d69a
Daniel <151851671+daniel-ultra@users.noreply.github.com>
2023-11-27 11:09:50 -0300

    Update and rename desafio03_aluraflix,js to desafio03_aluraflix.js

--- Aula 03/desafio03_aluraflix.js
+++ Aula 03/desafio03_aluraflix.js
@@ -3,22 +3,23 @@
 var listaFilmes = [
   "https://m.media-amazon.com/images/M/MV5BMTg3MTI5NTk0N15BMl5BanBnXkFtZTgwMjU1MDM5MTE@._V1_.jpg",
   "https://static.wixstatic.com/media/da06f2_9e0748f8e4fc45eba701ee2bd5581c11~mv2.jpg/v1/fill/w_640,h_998,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/da06f2_9e0748f8e4fc45eba701ee2bd5581c11~mv2.jpg",
-  "https://pics.filmaffinity.com/Napoleon-576065052-mmed.jpg",
-  "imagem_quebrada"
+  "https://pics.filmaffinity.com/Napoleon-576065052-mmed.jpg"
 ];
 
-var nomeFilmes = ["La Grande Illusion", "Arrival", ""]
+var nomeFilmes = ["La Grande Illusion", "Arrival", ""];
 
-var i = 0;
-while (i < listaFilmes.length) {
+for (var i = 0; i < listaFilmes.length; i++) {
   if (listaFilmes[i].endsWith("jpeg") || listaFilmes[i].endsWith("jpg")) {
     document.write("<img src=" + listaFilmes[i] + ">");
     document.write("<p>" + nomeFilmes[i] + "</p>");
-    if (!nomeFilmes[i] || nomeFilmes[i].trim() === "") {
-      document.write(" O nome do filme não foi inserido!<br>");
-    }
   } else {
-    document.write("<p> A imagem inserida não tem um formato válido para o filme: " + nomeFilmes[i] + "<br> </p>");
+    document.write(
+      "<p> A imagem " +
+        i +
+        " não foi lida pois não é um arquivo do tipo .jpeg ou .jpg."
+    );
   }
-  i++;
+  if ((nomeFilmes[i].trim() === "")) {
+    document.write("O nome do filme não foi inserido!");
+  }
 }
